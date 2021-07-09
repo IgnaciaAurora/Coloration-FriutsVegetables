1406f05318635cf82816b2214cea38718dcc7ff9
IgnaciaAurora <ignacialoy2@gmail.com>
2021-07-08 20:22:13 -0400

    Primera Version(Keras)

---
+++ Scripts/colorization.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+from IPython.display import display, Image
+from keras.layers import Conv2D, UpSampling2D, InputLayer
+from keras.models import Sequential
+from keras.preprocessing.image import img_to_array, load_img
+from skimage.color import lab2rgb, rgb2lab
+import matplotlib.pyplot as plt
+from pylab import *
+
+import os
+
+#print(os.listdir("C:/Users/ignac/OneDrive/Escritorio/Ignacia/Libros/Naruto/Naruto Tomo 07/Capitulo 55"))
+INPUT_IMAGE_SRC = "C:/Users/ignac/OneDrive/Escritorio/Ignacia/Libros/Naruto/Naruto Tomo 07/Capitulo 55/002.jpg"
+display(Image(INPUT_IMAGE_SRC,width=225))
+image = img_to_array(load_img(INPUT_IMAGE_SRC,target_size=(200,200)))/255
+lab_image = rgb2lab(image)
+print(lab_image.shape)
+lab_image_norm = (lab_image + [0, 128, 128]) / [100, 255, 255]
+# The input will be the black and white layer
+X = lab_image_norm[:,:,0]
+# The outpts will be the ab channels
+Y = lab_image_norm[:,:,1:]
+
+# The Conv2D layer we will use later expects the inputs and training outputs to be of the following format:
+# (samples, rows, cols, channels), so we need to do some reshaping
+# https://keras.io/layers/convolutional/
+X = X.reshape(1, X.shape[0], X.shape[1], 1)
+Y = Y.reshape(1, Y.shape[0], Y.shape[1], 2)
+
+model = Sequential()
+model.add(InputLayer(input_shape=(None, None, 1)))
+model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(2, (3,3), activation='tanh', padding='same'))
+
+# Finish model
+model.compile(optimizer='rmsprop', loss='mse')
+model.fit(x=X, y=Y, batch_size=1, epochs=1000, verbose=0)
+model.evaluate(X, Y, batch_size=1)
+output = model.predict(X)
+cur = np.zeros((200, 200, 3))
+cur[:,:,0] = X[0][:,:,0]
+cur[:,:,1:] = output[0]
+
+
+#imshow(rgb_image.astype('float32'))
+
+#cur = (cur * [100, 255, 255]) - [0 ,128, 128]
+#output
+
+#rgb_image *= [100, 255, 255]
+cur = (cur * [100, 255, 255]) - [0, 128, 128]
+rgb_image = lab2rgb(cur)
+imshow(rgb_image)
+#display(Image(rgb_image,width=225))
---
+++ Scripts/main.py
@@ -0,0 +1,73 @@
+#Parte 1 - Construir el modelo de CNN
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+#Inicializar la CNN
+classifier = Sequential()
+#Convolucion
+
+#Vamos a ocupar 32 filtros para detectar rasgos, el 3x3 definimos el tama;o
+#metodo add sirve apara a;adir una capa, en este caso la capa de convolution, no dense que se utiliza cuando se hace una rna mas generica
+classifier.add(Conv2D(filters=32,kernel_size=(3,3),input_shape=(64,64,3), activation="relu")) #Aqui agregamos una capa de convolucion
+#Es importante ver la documentacion
+#El formato o tamano de las imagenes input_shape, para especificar si los datos no vienen cuadrados, para que todas tengan el mismo formato
+#input_shape(255,255,1) en escala de grises
+#input_shape(128,128,3) en colores todo esto se puede modificar(es como el canal de color) (primero numero de filas, numero de columnas, numero de canales de color)
+
+#Paso 2 vamos con el maxpooling (reducimos el tama;o de la imagen)
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+#Flattening
+classifier.add(Flatten())
+#capa full conectada
+classifier.add(Dense(units= 128,activation = "relu")) #Esto es la capa oculta
+classifier.add(Dense(units = 1,activation="sigmoid")) #Esta es la capa de salida, como queremos en base a probabilidades ocupamos sigmoide (funciones de softmax o entropia cruzada en caso de que se ocupe mas de una categoira)
+#En este caso, es uno porque estamos usando una clasificacion binaria
+
+#Aqui estamos compilando la red neuronal
+#En este caso no estamos usando el gradiente grecedentiente sino el adam,
+#en loss es la funcion de perdida, en este caso estamos usando binary_crossen q es especial para clasificacion bianria, en caso de que hayan mas se utiliza otra
+#lo demas es la metrica de presicion
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+#Ajustamos la CNN  a las imagenes a entrenar
+
+from keras.preprocessing.image import ImageDataGenerator #Esto es para limpiar las imagenes y evitar el sobre ajuste
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('C:/Users/ignac/OneDrive/Escritorio/Ignacia/Redes Neuronales/Redes neuronales/deeplearning-az-master/datasets/Part 2 - Convolutional Neural Networks (CNN)/dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32, #Lotes de 32 imagenes
+                                                 class_mode = 'binary') #Yo se que tengo dos clases
+
+test_set = test_datagen.flow_from_directory('C:/Users/ignac/OneDrive/Escritorio/Ignacia/Redes Neuronales/Redes neuronales/deeplearning-az-master/datasets/Part 2 - Convolutional Neural Networks (CNN)/dataset/test_set',
+                                            target_size = (64, 64),#tama;o de carga
+                                            batch_size = 32,#tama;o de lotes de imagenes
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 250,#cuantas muestras tiene que tomar en un epoch completo
+                         epochs = 20,#cantidad de epoch q le dedicamos a la fase de entrenamiento
+                         validation_data = test_set,
+                         validation_steps = 1000)
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('C:/Users/ignac/OneDrive/Escritorio/Ignacia/Redes Neuronales/Redes neuronales/deeplearning-az-master/datasets/Part 2 - Convolutional Neural Networks (CNN)/dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+print(prediction)
